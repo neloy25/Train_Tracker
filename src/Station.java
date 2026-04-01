9fff1e068484146cf739d9c5f80f62e257cee154
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-04-01 23:38:52 +0600

    Station is updated

--- src/Station.java
+++ src/Station.java
@@ -1,5 +1,12 @@
-public interface Station {
-    String getarrivalTime();
-    String departureTime();
-    int getdelay();
+public class Station {
+    private  String stationName;
+    private  String arrivalTime;
+    private  String reverseArrivalTime;
+    private int waitTime;
+    public Station(String stationName,String arrivalTime,String reverseArrivalTime,int waitTime)
+    {
+        this.arrivalTime=arrivalTime;
+        this.stationName=stationName;
+        this.reverseArrivalTime= reverseArrivalTime;
+    }
 }
--- src/Train.java
+++ src/Train.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+
 public class Train {
-    private String TrainName ;
-    private int TrainNumber ;
-    private String StartStaion;
-    private  String Destination;
+    private String trainName ;
+    private int trainNumber ;
+    private String startStaion;
+    private  String destination;
+    private  String nextStaion;
+    private  String arrivalTime;
+    private  String  departureTime;
+    private  int delayTime;
+    ArrayList<Station> stations;
+
+
+    public  Train(int trainNumber,String trainName,String startStaion,String destination,
+                  ArrayList<Station>station )
+    {
+
+
+    }
+
+//    @Override
+//    public String getarrivalTime()
+//    {
+//        return  arrivalTime;
+//    }
+//
+//    @Override
+//    public String getDepartureTime() {
+//        return departureTime;
+//    }
+//
+//    @Override
+//    public int getdelay() {
+//        return delayTime;
+//    }
+
+
+
 
 
 }
